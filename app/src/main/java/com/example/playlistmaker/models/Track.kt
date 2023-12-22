213d0543b46f367746a116157d54013ca4746b34
Vaaargr <Antnsapstray@mail.ru>
2023-12-22 17:49:53 +0300

    Домашняя работа по спринту №14. 9я когорта. Сапегин Антон.

--- app/src/main/java/com/example/playlistmaker/models/Track.kt
+++ app/src/main/java/com/example/playlistmaker/models/Track.kt
@@ -12,7 +12,8 @@ data class Track(
     val collectionName: String?,
     val releaseDate: String,
     val primaryGenreName: String,
-    val country: String
+    val country: String,
+    val previewUrl: String
 ) : Serializable {
     fun getBigAlbumImage(): String? {
         return artworkUrl100?.replaceAfterLast('/', "512x512bb.jpg")
